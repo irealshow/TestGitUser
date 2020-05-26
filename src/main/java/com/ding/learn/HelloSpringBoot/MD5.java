c36a81d95b1dce12c34a678d7b4082aad2408a91
01482882 <01482882@yto.net.cn>
2020-05-26 21:09:05 +0800

    111

--- src/main/java/com/ding/learn/HelloSpringBoot/MD5.java
+++ src/main/java/com/ding/learn/HelloSpringBoot/MD5.java
@@ -36,6 +36,7 @@ public class MD5 {
     public static void main(String[] args) {
 		System.out.println(ToMD5("jrwMewH"));
 		System.out.println("0526123");
+		System.out.println("1111");
 	}
   
 }  
